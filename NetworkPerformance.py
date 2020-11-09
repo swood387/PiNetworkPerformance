a238428710ec9d7252610125cb64b1090e3a33ff
swood387 <33677630+swood387@users.noreply.github.com>
2020-11-08 21:24:57 -0500

    Update NetworkPerformance.py

--- NetworkPerformance.py
+++ NetworkPerformance.py
@@ -2,13 +2,8 @@ import subprocess
 import time
 import os
 
-import blinkt
-# Raspberry Pi pin configuration:
-RST = 24
 
-# 128x64 display with hardware I2C:
-
-#print 'Press Ctl-C to exit'
+print 'Press Ctl-C to exit'
 
 #functions
 def getSnmpData (oid):
@@ -40,37 +35,6 @@ def textRate (rate):
 	return result
 
 
-def show_graph(v):
-	#blinkt.clear()
-        #blinkt.set_brightness(0.04)
-        #blinkt.set_pixel(7,100,100,0)
-        #blinkt.show()
-        #time.sleep(.25)
-        blinkt.clear()
- 	blinkt.set_brightness(0.04)
-	#v = 100
-
-	num_pixels = int(round((v)/12))
-	print 'calculated pixels ' + str(num_pixels)
-	if (v > 0) and (num_pixels == 0):
-		num_pixels = 1
-
-	if num_pixels > 8:
-		num_pixels = 8
-	print 'actual pixels ' + str(num_pixels)
-
-	x = 0
-	while (x < num_pixels):
-		r, g, b = 0, 0, 155
-		if x > 4:
-			r, g, b = 155, 100, 0
-		if x > 6:
-			r, g, b = 155, 0, 0
-		print 'x = ' + str(x) + ' r = ' + str(r) + ' g = ' + str(g) + ' b = ' + str(b)
-		blinkt.set_pixel(x, r, g, b)
-		x = x + 1
-	blinkt.show()
-
 #defines
 oidInWan = '1.3.6.1.2.1.2.2.1.10.4'
 oidOutWan = '1.3.6.1.2.1.2.2.1.16.4'
@@ -79,8 +43,8 @@ lastInBytes = getSnmpInt (oidInWan);
 lastOutBytes = getSnmpInt (oidOutWan);
 lastTime = time.time()
 
-maxRateIn = 3750000
-maxRateOut = 120000
+maxRateIn = 12500000
+maxRateOut = 1250000
 
 #timed array vars
 timerTime = time.time()
@@ -144,13 +108,5 @@ while (1):
 	print 'In: ' + textRate(currInBytes) + '\tOut: ' + textRate(currOutBytes) + '\tElapsed ' + str(round(elapsed,2)) + '\t\tInMax ' + textRate(inMax) + '\tOutMax ' + textRate(outMax)
 	#print 'Hi In ' + str(highestSpeedIn) + '   Hi Out ' + str(highestSpeedOut)
 	print 'Download Percentage: ' + str(inPercentage) + '%' + '\tUpload Percentage: ' + str(outPercentage) + '%'
-
-	#
-	# Turn on Blinkit
-	#
-
-
-	graph_percentage = inPercentage
-	show_graph(graph_percentage)
 
 	time.sleep(5)
